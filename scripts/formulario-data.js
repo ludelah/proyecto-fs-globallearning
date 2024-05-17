04c4be6d9f55ed0b571784375b5a344bedd75597
Sebastian <sebascarp9@gmail.com>
2024-05-16 22:45:20 -0300

    script newCard finished

--- scripts/formulario-data.js
+++ scripts/formulario-data.js
@@ -1,26 +1,31 @@
-document.addEventListener('DOMContentLoaded', (event)=>{
-    let dataJSON = localStorage.getItem('dataNewAnimal');
-    if(dataJSON){
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    let dataJSON = localStorage.getItem('formDataArray');
+    if (dataJSON) {
         data = JSON.parse(dataJSON);
         console.log(data);
-    }else{
+    } else {
         data = [];
     }
-    let newCard = document.getElementsByClassName("animal-container");
+    let newCard = document.getElementById('animal-container');
+
     if (newCard) {
-        data.forEach((data,index) => {
-            let card = document.createElement("div");
+
+        data.forEach((data, index) => {
+            let card = document.createElement('div');
             card.className = "wrap-container";
             if (data.file) {
                 let imgContainer = document.createElement("div");
                 imgContainer.className = "img-container";
 
                 let imgAnimal = document.createElement("img");
-                imgAnimal.src = dato.file;
+                imgAnimal.src = data.file;
                 imgAnimal.alt = `Imangeb de ${data.animalName}`;
                 imgContainer.appendChild(imgAnimal);
                 card.appendChild(imgContainer);
             }
+
+
             let nameAnimal = document.createElement("h3");
             nameAnimal.className = "animal-name";
             nameAnimal.textContent = `${data.animalName}`;
@@ -31,17 +36,18 @@ document.addEventListener('DOMContentLoaded', (event)=>{
             dataAnimal.textContent = `${data.dataAnimal}`;
             card.appendChild(dataAnimal);
 
-            let nameUser = document - createElement("h3");
+            let nameUser = document.createElement("h3");
             nameUser.className = "user-name";
-            nameUser.textContent = `${nameUser}`;
+            nameUser.textContent = `${data.nameUser}`;
             card.appendChild(nameUser);
 
             let dataUser = document.createElement("p");
             dataUser.className = "user-data";
-            dataUser.textContent = `Contactame - Telefono:${tel} - Email: ${email}`;
+            dataUser.textContent = `Contactame - Telefono:${data.tel} - Email: ${data.email}`;
             card.appendChild(dataUser);
-
+           
             newCard.appendChild(card);
         });
+
     }
 })

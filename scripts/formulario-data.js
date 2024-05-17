137a30bef54b1799d24a848f03e20a8e046282ab
Sebastian <sebascarp9@gmail.com>
2024-05-16 21:15:37 -0300

    scripts files updates

---
+++ scripts/formulario-data.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+     document.getElementById('form',addEventListener('submit', createNewCard));
+})
+
+const createNewCard = (e) =>{
+    e.preventDefault();
+    
+    this.submit();
+}
--- scripts/formulario-validates.js
+++ scripts/formulario-validates.js
@@ -1,31 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('form').addEventListener('submit', createNewCard);
+    document.getElementById('form').addEventListener('submit', validateData);
 })
 
-const createNewCard = (e) => {
+const validateData = (e) => {
 
     e.preventDefault();
-
+    //Expresiones regulares para validar datos
     const regex = /^[a-zA-Z\s]*$/;
     const regexEmail = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi;
     const regexTel = /^\(?\d{2}\)?[\s\.-]?\d{4}[\s\.-]?\d{4}$/;
-
+    //Variables que contienen el valor de inputs
     const nameValue = document.getElementById('name');
     const name = nameValue.value.trim();
-    console.log(name);
 
     const nameAnimal = document.getElementById('animal');
     const animal = nameAnimal.value.trim();
-    console.log(animal);
 
     const emailValue = document.getElementById('email');
     const email = emailValue.value.trim();
-    console.log(email);
 
     const telValue = document.getElementById('telefono');
     const tel = telValue.value.trim();
-    console.log(tel);
 
+    const fileInput = document.getElementById('img');
+    const file = fileInput.files && fileInput.files[0];
+
+    //Funcion para validar datos y mostrar mensaje de error
     if (!regex.test(name) || !regex.test(animal) || !regexEmail.test(email) || !regexTel.test(tel)) {
         const errorMessage = document.getElementById('errorMessage')
         errorMessage.style.display = "block"
@@ -33,5 +33,24 @@ const createNewCard = (e) => {
     } else {
         errorMessage.style.display = "none";
     }
-    this.submit();
+    //Funcion para leer el archivo ingresado
+    if (file) {
+        const readFile = new FileReader();
+        readFile.onload = (e) => {
+            const dataNewAnimal = {
+                name: name,
+                animalName: animal,
+                email: email,
+                file: e.target.result
+            }
+            let formDataArray = JSON.parse(localStorage.getItem('formDataArray')) || [];
+            formDataArray.push(dataNewAnimal);
+            localStorage.setItem('formDataArray', JSON.stringify(formDataArray));
+            console.log('Data saved');
+        }
+        readFile.readAsDataURL(file);
+    } else {
+        alert('Insert a file valid')
+    }
+    window.location.reload();
 }
